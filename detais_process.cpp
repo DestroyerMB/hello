2ec386f390829691d66e65ea1091463d0e953208
DestroyerMB <vechernij.zvon@gmail.com>
2017-02-07 14:02:21 +0100

    Update detais_process.cpp

--- detais_process.cpp
+++ detais_process.cpp
@@ -1,29 +1,32 @@
 void DetailsProcess(CString input_file_name,CStringArray names,CStringArray starts,CStringArray stops,CString output_file_name)
 {
-  FILE *input_file=fopen(input_file_name,"r");
-  if(!input_file) return;
+  
+  FILE *input_file = _wfopen(connectFilePath, _T("rb")); 
+  if (!input_file) return; 
   
   CString data,result;
   
-  FILE *input_file = _wfopen(connectFilePath, _T("rb")); 
-  if (!fConnect) 
-    return; 
-  fseek(fConnect, 0, SEEK_END); 
-  lSize = ftell(fConnect); 
-  rewind(fConnect); 
+  fseek(input_file, 0, SEEK_END); 
+  lSize = ftell(input_file); 
+  rewind(input_file); 
 
   LPBYTE lpContent = (LPBYTE) malloc(lSize); 
-  fread(lpContent, 1, lSize, fConnect);
+  fread(input_file, 1, lSize, fConnect);
   fclose(lpContent);
+  data = lpContent;
   free(lpContent); 
-  
   
   for(int phrase=0;phrase<names.GetCount();phrase++)
   {
     CString start_phrase=starts[phrase];
     CString stop_phrase=stops[phrase];
-    int pos=()
-    
+    int start_pos=data.Find(start_phrase);
+    int stop_pos=data.Find(stop_phrase);
+    if(start_pos>=0 && stop_pos>=0 && stop_pos>start_pos)
+    {
+      if(!resut.IsEmpty()) result+=_T(",");
+      result+=data.Mid(start_pos,stop_pos-start_pos);
+    }
   }
   result+=_T("\n");
   
